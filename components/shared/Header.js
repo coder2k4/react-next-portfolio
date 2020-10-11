69d6e90b9d54e0257492eb2afaa92cce8570ceed
Standalone <coder2k@mail.ru>
2020-10-11 13:10:20 +0300

    auth init

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -25,10 +25,10 @@ const BsNavBrand = () =>
     </Link>
 
 const LoginLink = () =>
-    <span className="nav-link port-navbar-link clickable">Login</span>
+    <BsNavLink href="/api/v1/login" title={'login'}/>
 
 const LogoutLink = () =>
-    <span className="nav-link port-navbar-link clickable">Logout</span>
+    <BsNavLink href="/api/v1/logout" title={'logout'}/>
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
